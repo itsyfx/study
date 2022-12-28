5b74c6deacfe8a3bd30ee65a36b2c9bf1d2e9cd0
xianyu <983276928@qq.com>
2022-12-28 22:18:14 +0800

    --  redis模块增加swagger2 --

--- java-guide/src/main/java/com/demo/javaGuide/MainApplication.java
+++ java-guide/src/main/java/com/demo/javaGuide/MainApplication.java
@@ -4,8 +4,9 @@ package com.demo.javaGuide;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
+@EnableSwagger2
 @SpringBootApplication
 public class MainApplication {
 
--- java-guide/src/main/java/com/demo/javaGuide/controller/RedisController.java
+++ java-guide/src/main/java/com/demo/javaGuide/controller/RedisController.java
@@ -1,21 +1,26 @@
 package com.demo.javaGuide.controller;
 
 import com.demo.javaGuide.service.RedisService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "redis接口")
 @RestController
 public class RedisController {
 
     @Autowired
     private RedisService redisService;
 
-    @GetMapping("get2")
+    @GetMapping("/get")
+    @ApiOperation(value = "简单测试")
     public String getRedisData(){
         return redisService.getRedisData();
     }
 
+    @ApiOperation(value = "联通性测试")
     @GetMapping("/test")
     public void test(){
         System.out.println("xxx");
